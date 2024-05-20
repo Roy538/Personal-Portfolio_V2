0eeceb3e8667721814eb3190f3117ab7b3429f46
Roy538 <mauriceroy538@gmail.com>
2024-05-20 22:01:43 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+	// Sticky Navbar
+    let header = document.querySelector('header');
+    let menu = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+     
+     
+    window.addEventListener('scroll', () => {
+        header.classList.toggle('shadow', window.scrollY > 0);
+    });
+     
+    menu.onclick = () => {
+        navbar.classList.toggle('active');
+    }
+    window.onscroll = () => {
+        navbar.classList.remove('active');
+    }
+     
+    // Dark Mode
+    let darkmode = document.querySelector('#darkmode');
+     
+    darkmode.onclick = () => {
+        if(darkmode.classList.contains('bx-moon')){
+            darkmode.classList.replace('bx-moon','bx-sun');
+            document.body.classList.add('active');
+        }else{
+            darkmode.classList.replace('bx-sun','bx-moon');
+            document.body.classList.remove('active');
+        }
+    }
+
+    //typing animation 
+var typed = new Typed(".typing",{
+    strings: ["Web Development","Software Dev...","Graphic Design", "Mobile App Dev...", "DataBase Admin...","UI / UX Design", "IT specialist"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+var typed = new Typed(".typing-2",{
+    strings: ["Web Developer", "Mobile App Developer", "Programmer", "DataBase Administrator", "IT specialist"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+
+////tabs///////
+    var tablinks = document.getElementsByClassName("tab-links");
+    var tabcontents = document.getElementsByClassName("tab-contents");
+
+    function opentab(tabname){
+        for(tablink of tablinks){
+            tablink.classList.remove("active-link");
+        }
+        for(tabcontent of tabcontents){
+            tabcontent.classList.remove("active-tab");
+        }
+        event.currentTarget.classList.add("active-link");
+        document.getElementById(tabname).classList.add("active-tab");
+    }
