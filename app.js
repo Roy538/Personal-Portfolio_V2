12607b39c26e0baba875a80ff26560849fc82614
Roy538 <mauriceroy538@gmail.com>
2025-05-16 23:49:21 +0300

    16-May

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ class Particle {
       // if (mouse.y > this.y && this.y > this.size * 10){
         // this.y -=10;
       // }
-    // }
+    }
     // move particle
     this.x += this.directionX;
     this.y += this.directionY;
