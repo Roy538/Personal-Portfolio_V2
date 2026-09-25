34b9b933feca9094efe7f3c90ec58a3de6f73386
Roy538 <mauriceroy538@gmail.com>
2026-09-25 16:30:55 +0300

    Not working Fixes

--- script.js
+++ script.js
@@ -40,19 +40,6 @@ $('a[href*="#"]').on('click', function (e) {
 });
 
 
-/* ===== SCROLL REVEAL ANIMATION ===== */
-const srtop = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 1000,
-    reset: true
-});
-
-/* SCROLL EXPERIENCE */
-srtop.reveal('.experience .timeline',{delay: 400});
-srtop.reveal('.experience .timeline .container',{interval: 400}); 
-
-
 // Start of Tawk.to Live Chat
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
